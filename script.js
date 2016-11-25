24b8d70a455e0cd57ea0c2c5579bd3843dc1c095
nadapapa <nadapapa@gmail.com>
2016-11-25 17:07:37 +0100

    refactor + show number immediately after start

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 $.getJSON( "texts.json", function( data ) {
+  showRandomNumber(data);
   setInterval(function () {
-
-    var random = function (obj) {
-      var keys = Object.keys(obj);
-      return keys[ keys.length * Math.random() << 0];
-    };
-
-    var number = random(data);
-
-    var text = $("#texts"),
-        numberDom = $("#number");
-    text.html('<span>' + data[number] + '</span>');
-    text.textfill({
-      maxFontPixels: 200
-    });
-
-    numberDom.html(number).textfill({
-      maxFontPixels: 200
-    });
+    showRandomNumber(data);
   }, 30 * 1000);
-
 });
+
+function showRandomNumber(data) {
+  var number = random(data);
+
+  var text = $("#texts"),
+      numberDom = $("#number");
+  text.html('<span>' + data[number] + '</span>');
+  text.textfill({
+    maxFontPixels: 200
+  });
+
+  numberDom.html(number).textfill({
+    maxFontPixels: 200
+  });
+}
+
+function random(obj) {
+  var keys = Object.keys(obj);
+  return keys[ keys.length * Math.random() << 0];
+};
